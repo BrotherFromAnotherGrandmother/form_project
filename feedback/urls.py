05504c843705d1faaf5f7aae6d63212972a49401
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2022-09-29 20:19:44 +0300

    Перенаправил запрос с формы на роут "/hello"

--- feedback/urls.py
+++ feedback/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index, hello
 
 urlpatterns = [
     path('', index),
+    path('hello', hello),
 ]
