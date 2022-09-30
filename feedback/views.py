43dba33e85bad27c3777247915b56ac6e7c9b348
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2022-09-30 10:48:04 +0300

    Отлавливаю обработку метода POST, делаю редирект на роут done. Создал шаблон done. Создал функцию-отображение done.

--- feedback/views.py
+++ feedback/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        print(name)
+        return HttpResponseRedirect('/done')
     return render(request, 'feedback/feedback.html')
+
+def done(request):
+    return render(request, 'feedback/done.html')
