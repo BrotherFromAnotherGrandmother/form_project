d721482c738c67605f129589f078f1e0b0f25406
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2022-09-30 14:47:17 +0300

    Сделал валидацию формы по длине Создал новую форму для рейтинга. Валидировал её по величине

--- feedback/forms.py
+++ feedback/forms.py
@@ -2,6 +2,11 @@ from django import forms
 
 
 class FeedbackForm(forms.Form):
-    name = forms.CharField()
-    surname = forms.CharField()
-    feedback = forms.CharField(widget=forms.Textarea(attrs={'rows': 5, 'cols': 20}))
+    name = forms.CharField(label='Имя', max_length=7, min_length=2, error_messages={
+        'max_length': 'Слишком много символов',
+        'min_length': 'Слишком мало символов',
+        'required': 'Укажите хотя бы один символ',
+    })
+    # surname = forms.CharField()
+    # feedback = forms.CharField(widget=forms.Textarea(attrs={'rows': 5, 'cols': 20}))
+    rating = forms.IntegerField(label='рейтинг', max_value=5, min_value=1,)
