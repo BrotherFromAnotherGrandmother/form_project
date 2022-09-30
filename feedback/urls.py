d156dbcb9e96519b196b73229d54e44d8754df4c
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2022-09-30 09:53:12 +0300

    Проставил метод POST. Проставил токен. Удалил роут /hello. Удалил функцию hello.

--- feedback/urls.py
+++ feedback/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index, hello
+from .views import index
 
 urlpatterns = [
     path('', index),
-    path('hello', hello),
 ]
--- feedback/views.py
+++ feedback/views.py
@@ -4,6 +4,3 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'feedback/feedback.html')
-
-def hello(request):
-    return render(request, 'feedback/feedback.html')
