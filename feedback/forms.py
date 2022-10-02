c955b81871b2f159248f23a24ff20710f08d2a7d
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2022-10-02 16:08:32 +0300

    Создал форму FeedbackForm основанную на моделях

--- feedback/forms.py
+++ feedback/forms.py
@@ -1,12 +1,18 @@
 from django import forms
+from .models import Feedback
 
 
-class FeedbackForm(forms.Form):
-    name = forms.CharField(label='Имя', max_length=7, min_length=2, error_messages={
-        'max_length': 'Слишком много символов',
-        'min_length': 'Слишком мало символов',
-        'required': 'Укажите хотя бы один символ',
-    })
-    surname = forms.CharField()
-    feedback = forms.CharField(widget=forms.Textarea(attrs={'rows': 5, 'cols': 20}))
-    rating = forms.IntegerField(label='Рейтинг', max_value=5, min_value=1,)
+# class FeedbackForm(forms.Form):
+#     name = forms.CharField(label='Имя', max_length=7, min_length=2, error_messages={
+#         'max_length': 'Слишком много символов',
+#         'min_length': 'Слишком мало символов',
+#         'required': 'Укажите хотя бы один символ',
+#     })
+#     surname = forms.CharField()
+#     feedback = forms.CharField(widget=forms.Textarea(attrs={'rows': 5, 'cols': 20}))
+#     rating = forms.IntegerField(label='Рейтинг', max_value=5, min_value=1,)
+
+class FeedbackForm(forms.ModelForm):
+    class Meta:
+        model = Feedback
+        fields = ['name', 'rating']
