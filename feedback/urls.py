858449c02a97e43693c768ddbfb244aab7221212
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2022-10-05 19:05:33 +0300

    Создал класс DoneView с функцией get. Заменил им функцию-представление done. Удалил функцию update_feedback

--- feedback/urls.py
+++ feedback/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import done, FeedBackView, UpdateFeedback
+from .views import DoneView, FeedBackView, UpdateFeedback
 
 urlpatterns = [
-    path('done', done),
+    path('done', DoneView.as_view()),
     path('', FeedBackView.as_view()),
     path('<int:id_feedback>', UpdateFeedback.as_view()),
 ]
