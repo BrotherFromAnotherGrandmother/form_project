9a7620c4db3adee54f6fb39bedfcd5ee940821c8
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2022-10-05 19:27:49 +0300

    Заменил название класса UpdateFeedback на FeedBackUpdateView

--- feedback/urls.py
+++ feedback/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import DoneView, FeedBackView, UpdateFeedback
+from .views import DoneView, FeedBackView, FeedBackUpdateView
 
 urlpatterns = [
     path('done', DoneView.as_view()),
     path('', FeedBackView.as_view()),
-    path('<int:id_feedback>', UpdateFeedback.as_view()),
+    path('<int:id_feedback>', FeedBackUpdateView.as_view()),
 ]
--- feedback/views.py
+++ feedback/views.py
@@ -20,7 +20,7 @@ class FeedBackView(View):
         return render(request, 'feedback/feedback.html', context={'form': form})
 
 
-class UpdateFeedback(View):
+class FeedBackUpdateView(View):
     def get(self, request, id_feedback):
         feed = Feedback.objects.get(id=id_feedback)
         form = FeedbackForm(instance=feed)
