e5deda8e01455e1b32875e683a2427debf237806
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2022-10-06 16:37:22 +0300

    Создал класс от родительского ListView

--- feedback/views.py
+++ feedback/views.py
@@ -5,6 +5,7 @@ from .models import Feedback
 
 from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
 
 
 class FeedBackView(View):
@@ -47,13 +48,24 @@ class DoneView(TemplateView):
         return context
 
 
-class ListFeedBack(TemplateView):
-    template_name = 'feedback/list_feedback.html'
+# class ListFeedBack(TemplateView):
+#     template_name = 'feedback/list_feedback.html'
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['all_feed'] = Feedback.objects.all()
+#         return context
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['all_feed'] = Feedback.objects.all()
-        return context
+
+class ListFeedBack(ListView):
+    template_name = 'feedback/list_feedback.html'
+    model = Feedback
+    context_object_name = 'feedbacks'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        filter_qs = queryset.filter(rating__lt=4)
+        return filter_qs
 
 
 class DetailFeedBack(TemplateView):
